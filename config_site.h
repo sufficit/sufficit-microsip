7ae97bb1749ec48eec0b0d4d872336ccfa481bc9
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-12 19:55:49 -0300

    Create config_site.h

---
+++ config_site.h
@@ -0,0 +1,8 @@
+#define PJ_WIN32 1
+#include <windows.h>
+#define PJ_HAS_IPV6 1
+#define PJMEDIA_HAS_OPUS_CODEC 1
+#define PJMEDIA_OPUS_DEFAULT_BIT_RATE 32000
+#define PJ_LOG_MAX_LEVEL 4
+#define PJSUA_MAX_ACC 10
+#define PJSUA_MAX_CALLS 32
