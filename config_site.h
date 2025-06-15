4eb5602553fd86fba812048d1b9816b87e914ada
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-15 20:43:31 -0300

    patch

--- config_site.h
+++ config_site.h
@@ -2,7 +2,11 @@
 #define NOMINMAX // Prevent min/max macros from windows.h
 #define WIN32_LEAN_AND_MEAN // Reduce size of windows.h, preventing some conflicts
 #define PJ_HAS_STANDARD_CTYPE 1 // Use standard ctype.h functions instead of PJSIP wrappers
-// _WIN32_WINNT and WINVER will now be passed via MSBuild command line or handled by default SDK.
+
+// Disable PJSIP's internal definitions of WINVER and _WIN32_WINNT
+// This relies on the build environment/SDK setting these correctly via compiler flags.
+#define PJ_DONT_NEED_WIN32_VER_HACKS 1
+
 #include <windows.h>
 #define PJ_HAS_IPV6 1
 #define PJMEDIA_HAS_OPUS_CODEC 1
