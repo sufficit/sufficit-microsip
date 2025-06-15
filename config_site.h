64e31201b93db233177bd2bcd30bc3a7b2c64f49
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-15 20:03:12 -0300

    pacth

--- config_site.h
+++ config_site.h
@@ -1,4 +1,6 @@
 #define PJ_WIN32 1
+#define NOMINMAX // Prevent min/max macros from windows.h
+#define WIN32_LEAN_AND_MEAN // Reduce size of windows.h, preventing some conflicts
 #include <windows.h>
 #define PJ_HAS_IPV6 1
 #define PJMEDIA_HAS_OPUS_CODEC 1
