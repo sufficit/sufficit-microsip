9712d8252e10aeac865f61c6ea0a8ed658f37038
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-17 23:38:12 -0300

    Remove redundant _GLOBAL_VIDEO macro from const.h

--- const.h
+++ const.h
@@ -20,6 +20,6 @@
 #define _GLOBAL_VERSION "3.21.6"
 #define _GLOBAL_VERSION_COMMA 3,21,6,0
 #define _GLOBAL_KEY "*********"
-#define _GLOBAL_VIDEO
+// #define _GLOBAL_VIDEO
 #define _GLOBAL_NAME "MicroSIP"
 #define _GLOBAL_COMPANY "www.microsip.org"
